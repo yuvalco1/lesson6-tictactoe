852fde0baf4b87f9a89e81b13d2d2844f64fb2f5
Yuval Cohen <yuvalco1@gmail.com>
2023-11-25 09:52:47 +0200

    add getuserloc method

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,56 @@ public class Main {
     }
 
 
+    // get input location to fill from user and validate.
+
+    public static int[] getUserLoc(){
+        int [] userloc = new int[2];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter row number[1 to 3]: ");
+        int row = scanner.nextInt();
+        while (row<1 | row>3){
+            System.out.println("Row must be in range of 1 to 3, please re-enter");
+            row = scanner.nextInt();
+        }
+        System.out.println("please enter column number[1 to 3]: ");
+        int col = scanner.nextInt();
+        while (col<1 | col>3){
+            System.out.println("Column must be in range of 1 to 3, please re-enter");
+            col = scanner.nextInt();
+        }
+        userloc[0] = row;
+        userloc[1] = col;
+        return userloc;
+    }
+
+
+
+
+    // Check if matrix is full
+    public static boolean isMatrixFull(char[][] matrix) {
+        for (char[] row : matrix) {
+            for (char cell : row) {
+                if (cell == '_') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+    // Main program
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         char[][] matrix = createMatrix(3);
         printMatrix(matrix);
+        int[] user1loc = getUserLoc();
+        System.out.println(Arrays.toString(user1loc));
+
+
 
     }
 }
